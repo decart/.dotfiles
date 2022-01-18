4e14d636139cfd271a6d7211dea9f03e650cfe8b
mansur <yaponomat@gmail.com>
2022-01-18 17:04:50 +0300

    neovim: Config treesitter highlight

--- nvim/.config/nvim/lua/mansur/treesitter.lua
+++ nvim/.config/nvim/lua/mansur/treesitter.lua
@@ -2,7 +2,8 @@ require('nvim-treesitter.install').compilers = { "clang" }
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'all',
   highlight = {
-    enable = true
+    enable = true,
+    additional_vim_regex_highlighting = { 'vim' }
   },
   context_commentstring = {
     enable = true,
