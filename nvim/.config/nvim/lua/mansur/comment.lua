881108a5a6023ea79347c5df80a4e474a015e289
Мансур Лутфуллин <yaponomat@gmail.com>
2022-04-18 20:26:02 +0300

    nvim: Update commenting keymaps

--- nvim/.config/nvim/lua/mansur/comment.lua
+++ nvim/.config/nvim/lua/mansur/comment.lua
@@ -1,25 +1,38 @@
 require('Comment').setup {
   toggler = {
-    line = '<Leader>cc',
     block = '<Leader>bc'
   },
   opleader = {
-    line = '<Leader>c',
     block = '<Leader>b'
   },
   pre_hook = function(ctx)
-    local U = require 'Comment.utils'
+    -- Only calculate commentstring for tsx filetypes
+    if vim.bo.filetype == 'typescriptreact' then
+      local U = require('Comment.utils')
 
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require('ts_context_commentstring.utils').get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require('ts_context_commentstring.utils').get_visual_start_location()
+      -- Detemine whether to use linewise or blockwise commentstring
+      local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
+
+      -- Determine the location where to calculate commentstring from
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location = require('ts_context_commentstring.utils').get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location = require('ts_context_commentstring.utils').get_visual_start_location()
+      end
+
+      return require('ts_context_commentstring.internal').calculate_commentstring({
+        key = type,
+        location = location,
+      })
     end
-
-    return require('ts_context_commentstring.internal').calculate_commentstring {
-      key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
-      location = location,
-    }
   end
 }
+
+local opt = { expr = true, remap = true }
+
+-- Toggle using count
+vim.keymap.set('n', '<C-_>', '<CMD>lua require("Comment.api").toggle_current_linewise()<CR>')
+
+-- Toggle in VISUAL mode
+vim.keymap.set('x', '<C-_>', '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
