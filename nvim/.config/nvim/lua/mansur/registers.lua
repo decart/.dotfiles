f8d92632de285396f4c3a5db05d7961b8de21fcc
Мансур Лутфуллин <yaponomat@gmail.com>
2022-10-18 15:08:39 +0300

    nvim: Config registers plugin

--- nvim/.config/nvim/lua/mansur/registers.lua
+++ nvim/.config/nvim/lua/mansur/registers.lua
@@ -1,1 +1,7 @@
+require('registers').setup({
+  window = {
+    border = 'rounded'
+  }
+})
+
 vim.g.registers_paste_in_normal_mode = 1 -- automatically perform a paste action when selecting a register in normal mode
