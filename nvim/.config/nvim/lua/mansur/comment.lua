0281e006d098b8f048262ac45ad272cd96a6423d
Мансур Лутфуллин <yaponomat@gmail.com>
2022-11-30 19:31:11 +0300

    nvim: Update comment confgi

--- nvim/.config/nvim/lua/mansur/comment.lua
+++ nvim/.config/nvim/lua/mansur/comment.lua
@@ -1,38 +1,9 @@
 require('Comment').setup {
   toggler = {
-    block = '<Leader>bc'
+    line = '<C-_>',
   },
   opleader = {
-    block = '<Leader>b'
-  },
-  pre_hook = function(ctx)
-    -- Only calculate commentstring for tsx filetypes
-    if vim.bo.filetype == 'typescriptreact' then
-      local U = require('Comment.utils')
-
-      -- Detemine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.block then
-        location = require('ts_context_commentstring.utils').get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require('ts_context_commentstring.utils').get_visual_start_location()
-      end
-
-      return require('ts_context_commentstring.internal').calculate_commentstring({
-        key = type,
-        location = location,
-      })
-    end
-  end
+    line = '<C-_>'
+  }
 }
 
-local opt = { expr = true, remap = true }
-
--- Toggle using count
-vim.keymap.set('n', '<C-_>', '<CMD>lua require("Comment.api").toggle_current_linewise()<CR>')
-
--- Toggle in VISUAL mode
-vim.keymap.set('x', '<C-_>', '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
