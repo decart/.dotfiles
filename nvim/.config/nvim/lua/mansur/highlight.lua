85ef15c08e06d6343cefc90a68788eef7003033f
Мансур Лутфуллин <yaponomat@gmail.com>
2022-12-09 13:39:02 +0300

    nvim: Update highlights

--- nvim/.config/nvim/lua/mansur/highlight.lua
+++ nvim/.config/nvim/lua/mansur/highlight.lua
@@ -3,7 +3,9 @@ vim.g.enable_italic_font = 1
 vim.api.nvim_set_option('background', 'dark')
 vim.cmd('colorscheme hybrid_reverse')
 
-local bg = require('mansur/utils').bg
+local utils = require('mansur.utils')
+local bg = utils.bg
+local hi = utils.hi
 
 local bgNormal = bg('Normal')
 local bgInactive = '#26373c'
@@ -21,3 +23,7 @@ vim.cmd('hi BufferVisibleMod guibg=' .. bgInactive)
 vim.cmd('hi DiagnosticHint guifg=#666666')
 vim.cmd('hi DiagnosticError guifg=#F44336')
 vim.cmd('hi DiagnosticWarn guifg=#b27300')
+
+hi('NvimTreeGitDirty', { fg = "#f0c674" })
+hi('NvimTreeGitNew', { fg = "#b5bd68" })
+hi('NvimTreeGitDeleted', { link = "NvimTreeLspDiagnosticsError" })
--- nvim/.config/nvim/lua/mansur/utils.lua
+++ nvim/.config/nvim/lua/mansur/utils.lua
@@ -18,5 +18,30 @@ M.bg = function(group, default)
   return default
 end
 
+-- val table:
+--   fg (or foreground): color name or "#RRGGBB", see note.
+--   bg (or background): color name or "#RRGGBB", see note.
+--   sp (or special): color name or "#RRGGBB"
+--   blend: integer between 0 and 100
+--   bold: boolean
+--   standout: boolean
+--   underline: boolean
+--   undercurl: boolean
+--   underdouble: boolean
+--   underdotted: boolean
+--   underdashed: boolean
+--   strikethrough: boolean
+--   italic: boolean
+--   reverse: boolean
+--   nocombine: boolean
+--   link: name of another highlight group to link to, see |:hi-link|.
+--   default: Don't override existing definition |:hi-default|
+--   ctermfg: Sets foreground of cterm color |ctermfg|
+--   ctermbg: Sets background of cterm color |ctermbg|
+--   cterm: cterm attribute map, like |highlight-args|. If not set, cterm attributes will match those from the attribute map documented above.
+M.hi = function(name, val)
+  vim.api.nvim_set_hl(0, name, val)
+end
+
 
 return M
