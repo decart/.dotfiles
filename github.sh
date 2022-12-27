8d7d37a46483fef80f379401dca92402e982603a
Мансур Лутфуллин <yaponomat@gmail.com>
2022-12-27 12:10:13 +0300

    shell: Add install pnpm

--- github.sh
+++ github.sh
@@ -40,6 +40,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 NVM_DIR=$([ -z "${XDG_CONFIG_HOME-}" ] && echo "$HOME/.nvm" || echo "$XDG_CONFIG_HOME/nvm")
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 'lts/*'
+npm i -g pnpm
 
 # Remove created config files
 rm -f $HOME/.zshrc
