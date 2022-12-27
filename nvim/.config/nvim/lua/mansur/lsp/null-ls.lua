5d37bb043474c16800c27dc0f841217e8b740671
Мансур Лутфуллин <yaponomat@gmail.com>
2022-12-27 12:07:26 +0300

    nvim: Update null-ls config

--- nvim/.config/nvim/lua/mansur/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/mansur/lsp/null-ls.lua
@@ -1,7 +1,8 @@
-require('null-ls').setup({ 'stylua', 'jq', 'eslint_d', 'blade' })
+local servers = { 'stylua', 'jq', 'eslint_d', 'blade', 'shellcheck' }
+require('null-ls').setup(servers)
 
 require('mason-null-ls').setup({
-  ensure_installed = nil,
+  ensure_installed = servers,
   automatic_installation = true,
   automatic_setup = true
 })
