3476646d374b82592e98f3088d1591369ca70647
Мансур Лутфуллин <yaponomat@gmail.com>
2023-01-13 17:00:09 +0300

    nvim: Update cmp config

--- nvim/.config/nvim/lua/mansur/cmp-setup.lua
+++ nvim/.config/nvim/lua/mansur/cmp-setup.lua
@@ -6,28 +6,15 @@ local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 vim.o.completeopt = 'menuone,noselect'
 
 cmp.setup {
-
   formatting = {
     fields = { 'kind', 'abbr', 'menu' },
     format = function (entry, vim_item)
-      local formatter = lspkind.cmp_format({ 
-        mode = 'symbol_text',
-        maxwidth = 55,
-        menu = {
-          nvim_lsp = '[LSP]',
-          treesitter = '[TS]',
-          buffer = '[Buff]',
-          luasnip = '[Snip]',
-          nvim_lua = '[Lua]',
-          path = '[Path]',
-          nvim_lsp_signature_help = '[Help]',
-        }
-      })
+      local formatter = lspkind.cmp_format({ mode = 'symbol_text', preset = 'codicons', maxwidth = 45 })
+
       local kind = formatter(entry, vim_item)
       local strings = vim.split(kind.kind, '%s', { trimempty = true })
 
       kind.kind = " " .. (strings[1] or "") .. " "
-      -- kind.menu = "    (" .. (strings[2] or "") .. ") " .. (kind.menu or "")
       kind.menu = "    " .. (strings[2] or "")
 
       return kind
