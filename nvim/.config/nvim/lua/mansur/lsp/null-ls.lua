ae5059f0acd748a7f50bca7856e13e8b70ec9adb
Мансур Лутфуллин <yaponomat@gmail.com>
2023-01-20 09:10:45 +0300

    nvim: Update null-config for use eslint

--- nvim/.config/nvim/lua/mansur/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/mansur/lsp/null-ls.lua
@@ -1,10 +1,14 @@
 local null_ls = require('null-ls')
 null_ls.setup({
-  sources = { null_ls.builtins.formatting.fixjson }
+  sources = { 
+    null_ls.builtins.formatting.fixjson,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.eslint,
+  }
 })
 
 require('mason-null-ls').setup({
-  ensure_installed = { 'stylua', 'fixjson', 'eslint_d', 'blade', 'shellcheck' },
+  ensure_installed = { 'stylua', 'fixjson', 'blade', 'shellcheck' },
   automatic_installation = true,
   automatic_setup = true
 })
