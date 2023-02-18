b9d4556602f2843ae8b0726fb66e6e6faca43ab4
Мансур Лутфуллин <yaponomat@gmail.com>
2023-02-18 17:26:56 +0300

    nvim: Update keymaps

--- nvim/.config/nvim/lua/vscode/keymaps.lua
+++ nvim/.config/nvim/lua/vscode/keymaps.lua
@@ -5,8 +5,11 @@ local directions = require('hop.hint').HintDirection
 map('', '<Space>', '<Nop>')
 vim.g.mapleader = ' '
 
+
 -- Macros
 map('n', 'Q', '@@')
+
+map('n', 'U', '<C-r>')
 
 -- Indentation
 map('v', '>', '>gv')
@@ -20,22 +23,28 @@ map('v', 'x', '"_x')
 map('v', 'c', '"_c')
 map('v', 'D', '"_D')
 
+map('n', '?', ":call VSCodeNotify('actions.find')<CR>")
 map('n', '/', ':HopPattern<CR>')
 map('n', 's', ':HopChar1<CR>')
 
-map('', 'f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end)
+map('', 'f', function() hop.hint_char1({ current_line_only = true }) end)
+map('', 't', function() hop.hint_char1({ current_line_only = true, hint_offset  = -1 }) end)
 
-map('', 'F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end)
+-- Leader
+map('n', '<leader>sa', 'ggVG') -- Select all
+map('n', '<leader>fn', ":call VSCodeNotify('workbench.action.files.newUntitledFile')<CR>") -- New untitled file
+map('n', '<leader>fs', ":call VSCodeNotify('workbench.action.files.saveAll')<CR>") -- Save all
+map('n', '<leader>fr', ":call VSCodeNotify('workbench.action.openRecent')<CR>") -- Open recent
+map('n', '<leader>ft', ":call VSCodeNotify('workbench.action.files.showActiveFileInExplorer')<CR>") -- Show active file in explorer
 
-map('', 't', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset  = -1 })
-end)
+map('n', '<leader>cf', ":call VSCodeNotify('editor.action.formatDocument')<CR>") -- Format code
+map('n', '<leader>cl', ":call VSCodeNotify('workbench.action.editor.changeLanguageMode')<CR>") -- Change file language
+map('n', '<leader>ce', ":call VSCodeNotify('editor.action.organizeImports')<CR>") -- Organize imports
+map('n', '<leader>cr', ":call VSCodeNotify('editor.action.rename')<CR>") -- Rename symbol
+map('n', '<leader>cx', ":call VSCodeNotify('editor.action.refactor')<CR>") -- Refactor
+map('n', '<leader>cf', ":call VSCodeNotify('editor.action.formatDocument')<CR>") -- Format code
 
-map('', 'T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_of = 1 })
-end)
-
+map('n', '<leader>gc', ":call VSCodeNotify('git.commit')<CR>") -- Git commit
+map('n', '<leader>gp', ":call VSCodeNotify('git.push')<CR>") -- Git push
+map('n', '<leader>gl', ":call VSCodeNotify('git.pull')<CR>") -- Git pull
+map('n', '<leader>gs', ":call VSCodeNotify('workbench.view.scm')<CR>") -- Git status
