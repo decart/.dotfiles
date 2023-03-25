e9c7c1441a9f41b1ecead901f5f9646da5e85b46
Мансур Лутфуллин <yaponomat@gmail.com>
2023-03-25 19:26:54 +0300

    nvim: Config lualine

--- nvim/.config/nvim/lua/mansur/lualine.lua
+++ nvim/.config/nvim/lua/mansur/lualine.lua
@@ -1,27 +1,51 @@
+local filenameComponent = {
+  'filename',
+  path = 1,
+  symbols = { modified = '●', readonly = '', unnamed = '', newfile = '' }
+}
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'dracula-nvim',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
+    disabled_filetypes = {
+      winbar = { 'neo-tree', 'aerial' }
+    },
     always_divide_middle = true,
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'aerial', 'encoding', 'fileformat', 'filetype'},
+    lualine_b = {'branch', 'diff'},
+    lualine_c = { 'diagnostics' },
+    lualine_x = {
+      'encoding',
+      {
+        'fileformat',
+        symbols = { unix = 'LF', -- e712
+          dos = 'CRLF',  -- e70f
+          mac = '',  -- e711
+        }
+      },
+      'filetype'
+    },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
+  inactive_sections = nil,
+  winbar = {
+    lualine_c = {
+      { 'filetype', icon_only = true, separator = { left = '', right = ''} },
+      filenameComponent,
+      'aerial'
+    },
+  },
+  inactive_winbar  = {
+    lualine_c = {
+      { 'filetype', icon_only = true, separator = { left = '', right = ''} },
+      filenameComponent
+    },
   },
   tabline = {},
   extensions = {
