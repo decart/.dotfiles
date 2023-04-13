85d9db15670ed1b74bd789e760d85bea81fe42e3
Мансур Лутфуллин <yaponomat@gmail.com>
2023-04-13 21:14:10 +0300

    nvim: Use eslint instead eslint_d

--- nvim/.config/nvim/lua/mansur/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/mansur/lsp/null-ls.lua
@@ -2,12 +2,14 @@ local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.fixjson,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.eslint,
   },
   border = "rounded",
 })
 
 require('mason-null-ls').setup({
-  ensure_installed = { 'stylua', 'fixjson', 'blade', 'shellcheck', 'eslint_d' },
+  ensure_installed = { 'stylua', 'fixjson', 'blade', 'shellcheck' },
   automatic_installation = true,
   automatic_setup = true
 })
