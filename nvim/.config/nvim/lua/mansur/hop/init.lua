144966f0bd18520a4ac6819365486f75e81cb189
Мансур Лутфуллин <yaponomat@gmail.com>
2023-04-14 20:29:51 +0300

    nvim: Update hop config

--- nvim/.config/nvim/lua/mansur/hop/init.lua
+++ nvim/.config/nvim/lua/mansur/hop/init.lua
@@ -1,5 +1,6 @@
 
 require('hop').setup({
   jump_on_sole_occurrence = true,
+  case_insensitive = true,
   extensions = { 'mansur.hop.word-start' }
 })
--- nvim/.config/nvim/lua/mansur/hop/word-start.lua
+++ nvim/.config/nvim/lua/mansur/hop/word-start.lua
@@ -1,6 +1,21 @@
 local hop = require('hop')
 local options = require('mansur.hop.opts')
 
+local function starts_with_uppercase(s)
+  if #s == 0 then
+    return false
+  end
+
+  local f = s:sub(1, vim.fn.byteidx(s, 1))
+  -- if it’s a space, we assume it’s not uppercase, even though Lua doesn’t agree with us; I mean, Lua is horrible, who
+  -- would like to argue with that creature, right?
+  if f == ' ' then
+    return false
+  end
+
+  return f:upper() == f
+end
+
 local M = {}
 M.opts = {}
 
@@ -21,8 +36,13 @@ function M.hint_word_start(opts)
   end
 
   local regex = '\\<' .. char
+  if (vim.o.smartcase and not starts_with_uppercase(char)) or opts.case_insensitive then
+    regex = '\\c' .. regex
+  end
+
+
   hop.hint_with(
-    generator(jump_target.regex_by_case_searching(regex, false, opts)),
+    generator(jump_target.regex_by_searching(regex, false, opts)),
     opts
   )
 end
