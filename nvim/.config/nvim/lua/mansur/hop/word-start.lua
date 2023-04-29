a6ead70d9387d367f050a6a3b68c9d3e830363fd
Мансур Лутфуллин <yaponomat@gmail.com>
2023-04-29 20:14:22 +0300

    nvim: Update hop plugin

--- nvim/.config/nvim/lua/mansur/hop/word-start.lua
+++ nvim/.config/nvim/lua/mansur/hop/word-start.lua
@@ -1,5 +1,6 @@
 local hop = require('hop')
 local options = require('mansur.hop.opts')
+local jump_target = require('hop.jump_target')
 
 local function starts_with_uppercase(s)
   if #s == 0 then
@@ -19,23 +20,15 @@ end
 local M = {}
 M.opts = {}
 
-function M.hint_word_start(opts)
-  local jump_target = require('hop.jump_target')
+M.hint_letter_start = function (char, opts)
   opts = options.override_opts(opts, M.opts)
 
-  local char = hop.get_input_pattern('Hop 1 char: ', 1)
-  if not char then
-    return
-  end
-
-  local generator
-  if opts.current_line_only then
-    generator = jump_target.jump_targets_for_current_line
-  else
-    generator = jump_target.jump_targets_by_scanning_lines
-  end
-
   local regex = '\\<' .. char
+  local generator = opts.current_line_only and
+    jump_target.jump_targets_for_current_line or
+    jump_target.jump_targets_by_scanning_lines
+
+
   if vim.o.smartcase then
     if not starts_with_uppercase(char) then
       regex = '\\c' .. regex
@@ -44,11 +37,35 @@ function M.hint_word_start(opts)
     regex = '\\c' .. regex
   end
 
-
   hop.hint_with(
     generator(jump_target.regex_by_searching(regex, false, opts)),
     opts
   )
+end
+
+M.hint_non_letter = function(char, opts)
+  opts = options.override_opts(opts, M.opts)
+
+  local generator = opts.current_line_only and
+    jump_target.jump_targets_for_current_line or
+    jump_target.jump_targets_by_scanning_lines
+
+  hop.hint_with(
+    generator(jump_target.regex_by_case_searching(char, true, opts)),
+    opts
+  )
+end
+
+function M.hint_word_start(opts)
+  local char = hop.get_input_pattern('Hop 1 char: ', 1)
+  if not char then return end
+
+  -- Check if char is letter
+  if string.match(char, '[%a%d_]') then
+    M.hint_letter_start(char, opts)
+  else
+    M.hint_non_letter(char, opts)
+  end
 end
 
 function M.register(opts)
