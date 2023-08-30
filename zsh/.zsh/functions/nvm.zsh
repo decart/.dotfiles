8e69abb314a0bf281271ca114cd9e9fef6c305fa
Мансур Лутфуллин <yaponomat@gmail.com>
2023-08-30 09:11:16 +0300

    shell: Add update nvm function

---
+++ zsh/.zsh/functions/nvm.zsh
@@ -0,0 +1,11 @@
+function nvm::update() {
+  local -r current_node_version=$(nvm current)
+  local -r next_node_version=$(nvm version-remote --lts)
+  if [ "$current_node_version" != "$next_node_version" ]; then
+    local -r previous_node_version=$current_node_version
+    nvm install --lts
+    nvm reinstall-packages "$previous_node_version"
+    nvm uninstall "$previous_node_version"
+    nvm cache clear
+  fi
+}
