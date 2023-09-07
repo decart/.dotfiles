328870302f1424d71a072169b90bedf750afe5d7
Мансур Лутфуллин <yaponomat@gmail.com>
2023-09-07 18:42:02 +0300

    shell: Change title of window depends on application

---
+++ zsh/.zsh/functions/hooks.zsh
@@ -0,0 +1,19 @@
+preexec() {
+  if [[ $2 =~ ^(n?vim)\|(nano)\|(lazygit) ]]; then
+    cmd=$(echo $2 | awk '{print $1;}')
+
+    if [ -n "$TMUX" ]; then
+      tmux -2u rename-window "$cmd [#{b:pane_current_path}]";
+    else
+      echo -ne "\033]0;$cmd [$(basename ${PWD})]\007"
+    fi
+  fi
+}
+
+precmd() {
+  if [ -n "$TMUX" ]; then
+    tmux -2u set-window-option automatic-rename "on" > /dev/null;
+  else
+    echo -ne "\033]0;$(basename ${PWD})\007"
+  fi
+}
--- zsh/.zsh/functions/ssh.zsh
+++ zsh/.zsh/functions/ssh.zsh
@@ -1,9 +1,13 @@
 ssh() {
+  local host=$(echo $* | rev | cut -d '@' -f1 | rev)
+
   if [ -n "$TMUX" ]; then
-    tmux -2u rename-window "ssh[$(echo $* | rev | cut -d '@' -f1 | rev)]";
+    tmux -2u rename-window "ssh[$host]";
     command ssh "$@";
     tmux -2u set-window-option automatic-rename "on" > /dev/null;
   else
+    echo -ne "\033]0;ssh[${host}]\007"
     command ssh "$@";
+    echo -ne "\033]0;$(basename ${PWD})\007"
   fi
 }
