f6a3449b1188fa4ef1066e8399b001a234df7cba
Мансур Лутфуллин <yaponomat@gmail.com>
2023-09-20 11:59:10 +0300

    shell: Update ssh title

--- zsh/.zsh/functions/ssh.zsh
+++ zsh/.zsh/functions/ssh.zsh
@@ -1,12 +1,13 @@
 ssh() {
   local host=$(echo $* | rev | cut -d '@' -f1 | rev)
+  local user=$(command ssh -G "$*" | grep -i -w -E '^user' | awk '{print $2}')
 
   if [ -n "$TMUX" ]; then
-    tmux -2u rename-window "ssh[$host]";
+    tmux -2u rename-window "ssh[$user@$host]";
     command ssh "$@";
     tmux -2u set-window-option automatic-rename "on" > /dev/null;
   else
-    echo -ne "\033]0;ssh[${host}]\007"
+    echo -ne "\033]0;ssh[${user}@${host}]\007"
     command ssh "$@";
     echo -ne "\033]0;$(basename ${PWD})\007"
   fi
