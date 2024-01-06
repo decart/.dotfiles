3979b10c2443dad54dc53b14c85722ba6803767c
Мансур Лутфуллин <yaponomat@gmail.com>
2024-01-06 13:08:09 +0300

    install: Add tools installation script

---
+++ tools/flatpak.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pkgs=$(\cat ~/.dotfiles/tools/flatpak-list.txt | column -ts '|' | fzf --margin=1,10% -m --layout=reverse | awk -F'[[:space:]][[:space:]]+' '{print $2}')
+
+if [[ -n "$pkgs" ]]; then
+	cmd="/usr/bin/flatpak install -y flathub $pkgs"
+	eval $cmd
+fi
