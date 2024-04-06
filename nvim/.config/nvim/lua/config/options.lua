893cc4f8db466ae0a86c85cde8d376cc7c499f74
Мансур Лутфуллин <yaponomat@gmail.com>
2024-04-06 22:53:56 +0300

    nvim: Config whitespace

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
+local opt = vim.opt
+
+opt.listchars = {
+  lead = "·",
+  tab = "▷-",
+  trail = "·",
+  extends = "◣",
+  precedes = "◢",
+  nbsp = "○",
+  -- eol = "⤶",
+}
+opt.list = true
